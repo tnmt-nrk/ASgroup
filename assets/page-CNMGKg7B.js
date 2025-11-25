a25cbbe3024858b5c97ff253ea73fcba3efad1b9
tnmt-nrk <tnmt.nrk@gmail.com>
2025-11-25 17:16:15 +0900

    first commit~

---
+++ assets/page-CNMGKg7B.js
@@ -0,0 +1,2 @@
+import{u as t,r as a}from"./index-CQCmJ27i.js";function o(){const e=t();return a.useEffect(()=>{e("/login",{replace:!0})},[e]),null}export{o as default};
+//# sourceMappingURL=page-CNMGKg7B.js.map
